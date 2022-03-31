af20d8ffaf077be3f3241d6d5ba8b0f6ae13103e
rody josue <rody.josue.cc.c@gmail.com>
2022-03-31 17:19:06 -0500

    add component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import '../styles/Header.scss';
+
+const Header=()=>{
+    return(
+            <header className="header">
+                <div className='header__banner-container'>
+                    <h3 className='header__banner'>DELIVERY GRATIS DESDE S/. 100</h3>
+                </div>
+                <div>
+
+                </div>
+                <nav className='header-nav'>
+                    <img src="" alt="menu" className="menu"/>
+                    <div className="navbar-left">
+                        <img src=" " alt="logo" className="logo"/>
+                        <ul>
+                            <li>
+                                <a href="/">Todo</a>
+                            </li>
+                            <li>
+                                <a href="/">Productos</a>
+                            </li>
+                            <li>
+                                <a href="/">Marcas</a>
+                            </li>
+                            <li>
+                                <a href="/">Reviews</a>
+                            </li>
+                            <li>
+                                <a href="/">Nosotros</a>
+                            </li>
+                            <li>
+                                <a href="/">Otros</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navbar-right">
+                        <ul>
+                            <li className="navbar-right__email">artesanershop@example.com</li>
+                            <li className="navbar-right__shopping-cart">
+                            <img className='navbar-right__icon-shopping' src="./icons/icon_shopping_cart.svg" alt="shopping cart"/>
+                            <div>2</div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+
+    );
+}
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Home=()=>{
 
     return(
-        <div>
-            <h1>Artesaner Shop</h1>
-        </div>
+        <Header/>
     );
 }
 export default Home;
