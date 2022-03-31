c7e3cb03c4816b6d4dec4d7e44666f7e74292d11
rody josue <rody.josue.cc.c@gmail.com>
2022-03-31 09:00:48 -0500

    add component login

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path'); //path del proyecto principal
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //traemos el plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+//de html
+
+module.exports = {
+  
+    entry: './src/index.js', // punto de entrada
+    output: { // lugar al que saldrán todos los archivos
+        path: path.resolve(__dirname, 'dist'), //en nuestro path, crea la carpeta dist
+        filename: 'bundle.js' // nombre del archivo js resultante
+    },
+    mode:'development',
+    resolve: { // extensión de archivos a tomar en cuenta
+        extensions: ['.js', '.jsx']
+    },
+    module: { // loaders para cada tipo de archivo
+        rules: [ // reglas para usar
+            {
+                test: /\.(js|jsx)$/, // extensiones en las cuales actuará babel
+                exclude: /node_modules/, // siempre excluir node modules 
+                use: { // indicamos el loader
+                    loader: 'babel-loader' // babel 
+                }
+            },
+            {
+                test: /\.html$/, // extensiones html
+                use: [
+                    {
+                        loader: 'html-loader' // loader a usar
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+         },
+        ]
+    },
+    plugins: [ // plugins 
+        new HtmlWebpackPlugin({ // instanciamos el plugin para html 
+            template: './public/index.html', // archivo raíz a transformar
+            filename: './index.html' // el archivo resultante
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3005,
+    },
+}
