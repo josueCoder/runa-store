d4eb3cb0c539788f2766052135b6d7362d2d8af6
rody josue <rody.josue.cc.c@gmail.com>
2022-04-02 20:26:22 -0500

    añadiendo  assets al componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
-import '../styles/Header.scss';
 
+import '@styles/Header.scss';
+
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_runa_sale.png';
+import shopingCart from '@icons/icon_shopping_cart.svg';
 
 
 
@@ -14,9 +18,9 @@ const Header=()=>{
 
                 </div>
                 <nav className='header-nav'>
-                    <img src="" alt="menu" className="menu"/>
+                    <img src={menu} alt="menu" className="menu"/>
                     <div className="navbar-left">
-                        <img src="" alt="logo" className="nav-logo"/>
+                        <img src={logo} alt="logo" className="nav-logo"/>
                         <ul>
                             <li>
                                 <a href="/">Todo</a>
@@ -42,7 +46,7 @@ const Header=()=>{
                         <ul>
                             <li className="navbar-right__email">artesaner@example.com</li>
                             <li className="navbar-right__shopping-cart">
-                            <img className='navbar-right__icon-shopping' src="" alt="shopping cart"/>
+                            <img className='navbar-right__icon-shopping' src={shopingCart} alt="shopping cart"/>
                             <div>2</div>
                             </li>
                         </ul>
