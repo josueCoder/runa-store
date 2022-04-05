71aad2f7c02f5b1bb385ea1115872126216743fa
rody josue <rody.josue.cc.c@gmail.com>
2022-04-05 07:57:18 -0500

    add acumulador de productos al carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_runa_sale.png';
+import AppContex from '../context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 
 
 const Header=()=>{
     const [toggle,setToggle]=useState(false);
+    const {state}=useContext(AppContex);
     const handleToggle=()=>{
         setToggle(!toggle);
     }
@@ -53,7 +55,7 @@ const Header=()=>{
                             </li>
                             <li className="navbar-right__shopping-cart">
                             <img className='navbar-right__icon-shopping' src={shoppingCart} alt="shopping cart"/>
-                            <div>2</div>
+                                {state.cart.length > 0 ?<div>{state.cart.length}</div>:null}
                             </li>
                         </ul>
                     </div>
