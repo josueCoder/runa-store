8b02aca0c180e2a467754d76b2025c4a4aacf47b
rody josue <rody.josue.cc.c@gmail.com>
2022-04-05 09:00:52 -0500

    add my orders a la tienda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React,{useState,useContext} from 'react'
 
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
+import MyOrder from '@containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_runa_sale.png';
 import AppContex from '../context/AppContext';
@@ -11,6 +12,7 @@ import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header=()=>{
     const [toggle,setToggle]=useState(false);
+    const [toggleOrders,setToggleOrders]=useState(false);
     const {state}=useContext(AppContex);
     const handleToggle=()=>{
         setToggle(!toggle);
@@ -53,13 +55,14 @@ const Header=()=>{
                             <li className="navbar-right__email" onClick={handleToggle}>
                                 runastore@gmail.com
                             </li>
-                            <li className="navbar-right__shopping-cart">
+                            <li className="navbar-right__shopping-cart" onClick={()=>setToggleOrders(!toggleOrders)}>
                             <img className='navbar-right__icon-shopping' src={shoppingCart} alt="shopping cart"/>
                                 {state.cart.length > 0 ?<div>{state.cart.length}</div>:null}
                             </li>
                         </ul>
                     </div>
                     {toggle && <Menu/>}
+                    {toggleOrders && <MyOrder/>}
                 </nav>
             </header>
 
