658f25853d97973b45c6479cbf8a5279a8e2527d
rody josue <rody.josue.cc.c@gmail.com>
2022-04-08 23:13:46 -0500

    update pages create account

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -5,17 +5,17 @@ const CreateAccount = () => {
 	return (
 		<div className="CreateAccount">
 			<div className="CreateAccount-container">
-				<h1 className="title">Mi cuenta</h1>
-				<form action="/" className="form">
-					<div>
-						<label for="name" className="label">Nombre</label>
-						<input type="text" id="name" placeholder="Teff" className="input input-name" />
-						<label for="email" className="label">Correo</label>
-						<input type="text" id="email" placeholder="runaestore@gmail.com" className="input input-email" />
-						<label for="password" className="label">Contraseña</label>
-						<input type="password" id="password" placeholder="*********" className="input input-password" />
+				<h1 className="create-account__title">Mi cuenta</h1>
+				<form action="/" className="create-account__form">
+					<div className='create-account__form-container'>
+						<label for="name" className="create-account__form-label">Celular</label>
+						<input type="text" id="name"  className="create-account__form-input" />
+						<label for="email" className="create-account__form-label">Correo</label>
+						<input type="text" id="email" placeholder="runaestore@gmail.com" className="create-account__form-input" />
+						<label for="password" className="create-account__form-label">Contraseña</label>
+						<input type="password" id="password" placeholder="*********" className="create-account__form-input" />
 					</div>
-					<input type="submit" value="Create" className="primary-button login-button" />
+					<input type="submit" value="Create" className="create-account__form-button login-button" />
 				</form>
 			</div>
 		</div>
